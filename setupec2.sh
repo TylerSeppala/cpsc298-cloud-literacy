cefff339019b78b378f0f68ebaea649a3eb4a460
Tyler Seppala <trseppala@hotmail.com>
2021-03-10 20:53:49 -0800

    Fixed bash file

--- setupec2.sh
+++ setupec2.sh
@@ -4,7 +4,7 @@ sudo yum groupinstall 'Development Tools' -y
 sudo yum install httpd -y
 sudo mkdir /wcode
 cd /wcode
-sudo git clone --branch seppala https://github.com/harteratchapman/cpsc298-cloud-literacy.git
+sudo git clone https://github.com/TylerSeppala/cpsc298-cloud-literacy.git
 sudo service httpd start
 sudo chkconfig httpd on
 cd /wcode/cpsc298-cloud-literacy/renderer/raytrace
