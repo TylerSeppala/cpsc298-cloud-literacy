abc9bb800943417b68d9a381ee9f5cc60e9f03db
Tyler Seppala <trseppala@hotmail.com>
2021-03-10 21:35:26 -0800

    Fixed shell again

--- setupec2.sh
+++ setupec2.sh
@@ -10,6 +10,6 @@ sudo chkconfig httpd on
 cd /wcode/cpsc298-cloud-literacy/renderer/raytrace
 sudo ./build
 sudo ./raytrace test
-sudo cp /wcode/cpsc298-cloud-literacy/index/index.html /var/www/html
+sudo cp /wcode/cpsc298-cloud-literacy/index/index.html /var/www/index.html
 sudo cp block.png /var/www/html/myshape.png
 
