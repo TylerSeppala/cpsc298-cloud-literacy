e41b0201defb56d0aac93bb85847d771dc8670b7
Tyler Seppala <trseppala@hotmail.com>
2021-03-10 21:43:53 -0800

    Re-arranged files

--- setupec2.sh
+++ setupec2.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/bash
-sudo yum update -y
-sudo yum groupinstall 'Development Tools' -y
-sudo yum install httpd -y
-sudo mkdir /wcode
+yum update -y
+yum groupinstall 'Development Tools' -y
+yum install httpd -y
+mkdir /wcode
 cd /wcode
-sudo git clone https://github.com/TylerSeppala/cpsc298-cloud-literacy.git
-sudo service httpd start
-sudo chkconfig httpd on
+git clone https://github.com/TylerSeppala/cpsc298-cloud-literacy.git
+service httpd start
+chkconfig httpd on
 cd /wcode/cpsc298-cloud-literacy/renderer/raytrace
-sudo ./build
-sudo ./raytrace test
-sudo cp /wcode/cpsc298-cloud-literacy/index/index.html /var/www/index.html
-sudo cp block.png /var/www/html/myshape.png
-
+./build
+./raytrace block
+cp block.png /var/www/html/myshape.png
+cp /wcode/cpsc298-cloud-literacy/index.html /var/www/html/index.html
